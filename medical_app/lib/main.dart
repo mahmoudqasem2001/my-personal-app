a2883292a0954ad73f025c7775f856e85a32e836
Mahmoud <mahmoud.qasem@endeavorpal.com>
2023-10-20 21:52:57 +0300

    edit main.dart file

--- medical_app/lib/main.dart
+++ medical_app/lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("hello"),
+      ),
+    ));
   }
 }
